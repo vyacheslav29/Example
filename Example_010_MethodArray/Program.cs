bd78501e62dc6d0e7bbd54bd0f534d82e63accf1
Вячеслав <vyacheslav.ivanov.r38@mail.ru>
2022-08-12 16:03:25 +0900

    Нахождения индекса в масиве заданного числа этого же массива

---
+++ Example_010_MethodArray/Program.cs
@@ -0,0 +1,18 @@
+﻿// Нахождения индекса в масиве заданного числа этого же массива:
+
+int[] array = { 1, 12, 31, 4, 18, 15, 16, 17, 18 };
+
+int n = array.Length; //Определяет длинну массива.
+int find = 18;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++; // Счетчик +1 шаг.
+}
